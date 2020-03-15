3a47f1c84f5f62fcac78261b084f9f5f299dd9f9
xingchen <784172167@qq.com>
2020-03-16 00:25:53 +0800

    V1.0.0 基于springboot的简易交友系统

---
+++ src/main/java/com/paqi/friendsystem/FriendsystemApplication.java
@@ -0,0 +1,13 @@
+package com.paqi.friendsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriendsystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/paqi/friendsystem/FriendsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paqi.friendsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
