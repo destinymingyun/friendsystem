8fce28a8b79a8c82fd90085fca5f17cbe4d7226c
xingchen <784172167@qq.com>
2020-03-16 14:20:19 +0800

    V2.0.4   Create UserSafetyMapper 新增用户安全添加问题

---
+++ src/main/java/com/paqi/friendsystem/mappertest/UserSafetyMapper.java
@@ -0,0 +1,27 @@
+package com.paqi.friendsystem.mappertest;
+
+import com.paqi.friendsystem.entity.user.UserSafety;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author PQ
+ * @Description 用户安全信息类，用于密码修改
+ * @Date 下午1:13 16/3/2020
+ * @version 2.0.4
+**/
+@Mapper
+public interface UserSafetyMapper {
+    /**
+     * 向数据库中user_safety添加一条信息
+     * @author PQ
+     * @Description 添加用户安全信息
+     * @param userSafety：用户安全问题
+     * @return 返回数据库受影响的行数
+     * @Date 下午1:14 16/3/2020
+     * @version 2.0.4
+    **/
+    @Insert("INSERT INTO `user_safety` (`user_id`, `question1`, `question2`, `question3`, `answer1`, `answer2`, `answer3`)" +
+            " VALUES (#{userId}, #{question1}, #{question2}, #{question3}, #{answer1}, #{answer2}, #{answer3})")
+    int postUserMSafety(UserSafety userSafety);
+}
---
+++ src/test/java/com/paqi/friendsystem/mappertest/UserSafetyMapperTest.java
@@ -0,0 +1,39 @@
+package com.paqi.friendsystem.mappertest;
+
+import com.paqi.friendsystem.entity.user.UserSafety;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author PQ
+ * @Description 测试UserSafetyMapper是否可运行
+ * @Date 下午1:22 16/3/2020
+ * @version 2.0.4
+**/
+@SpringBootTest
+public class UserSafetyMapperTest {
+    @Autowired
+    private UserSafetyMapper userSafetyMapper;
+
+    /**
+     * @author PQ
+     * @Description 测试是否能添加一条数据库记录
+     * @Date 下午2:14 16/3/2020
+     * @version 2.0.4
+    **/
+    @Test
+    public void testPostUserSafetyMapper() {
+        UserSafety userSafety = new UserSafety();
+        userSafety.setUserId(1);
+        userSafety.setQuestion1("q1");
+        userSafety.setQuestion2("q2");
+        userSafety.setQuestion3("q3");
+        userSafety.setAnswer1("a1");
+        userSafety.setAnswer2("a2");
+        userSafety.setAnswer3("a3");
+        int ret = userSafetyMapper.postUserMSafety(userSafety);
+        Assertions.assertEquals(1, ret);
+    }
+}
